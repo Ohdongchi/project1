30d7f0265151b0af440a013967369b29aaf741f0
Ohdongchi <dhqud1234@naver.com>
2020-07-16 02:09:36 +0900

    project1 end commit 2

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ app.use((req, res, next) => {
 
 app.use((err, req, res, next) => {
     res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.locals.error = req.app.get('env') === 'production' ? err : {};
     res.status(err.status || 500);
     res.render('error');
 });
